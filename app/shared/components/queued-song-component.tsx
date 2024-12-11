b38c3ee451133e1f1215d1b5454877eb211dbb8a
Dorian TETU <doriantetu34@gmail.com>
2024-12-11 10:05:07 +0100

    Add internationalization support to PendingSongItem and QueuedSongItem components; update button labels and locale detection

--- app/shared/components/queued-song-component.tsx
+++ app/shared/components/queued-song-component.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { useTranslations } from 'next-intl';
 
 interface SongItemProps {
     id: string;
@@ -9,6 +10,7 @@ interface SongItemProps {
 }
 
 export function QueuedSongItem({id, title, artists, time, handleDel}: SongItemProps) {
+    const t = useTranslations('button');
     return (
         <li key={id} className='flex flex-row items-center px-5 h-[100px] w-[300px] lg:w-[600px] rounded-xl my-4 border hover:border-black justify-between'>
             <div className='flex flex-col pr-5'>
@@ -17,7 +19,7 @@ export function QueuedSongItem({id, title, artists, time, handleDel}: SongItemPr
             </div>
             <div className='flex items-center gap-2'>
                 <p className='hidden lg:block text-gray-400 text-sm px-2'>{time && new Date(time).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}</p>
-                <Button onClick={() => handleDel(id)}>Delete</Button>
+                <Button onClick={() => handleDel(id)}>{t('delete')}</Button>
             </div>
         </li>
     )
