e7615814dd6374e574971578690817a37868e96b
Dorian TETU <doriantetu34@gmail.com>
2024-12-11 10:05:07 +0100

    Add internationalization support to PendingSongItem and QueuedSongItem components; update button labels and locale detection

--- app/shared/components/pending-song-component.tsx
+++ app/shared/components/pending-song-component.tsx
@@ -1,6 +1,7 @@
 
 import { Button } from '@/components/ui/button'
 import { Minus, Plus } from 'lucide-react';
+import { useTranslations } from 'next-intl';
 
 interface SongItemProps {
     id: string;
@@ -13,6 +14,7 @@ interface SongItemProps {
 }
 
 export function PendingSongItem({id, title, artists, time, handleQueue, handleReject}: SongItemProps) {
+    const t = useTranslations('button');
     return (
         <li key={id} className='flex flex-row items-center px-5 h-[100px] w-[300px] lg:w-[600px] rounded-xl my-4 border hover:border-black justify-between'>
             <div className='flex flex-col pr-5 truncate'>
@@ -21,8 +23,8 @@ export function PendingSongItem({id, title, artists, time, handleQueue, handleRe
             </div>
             <div className='flex items-center gap-2 pl-5'>
                 <p className='hidden lg:block text-gray-400 text-sm px-2'>{time && new Date(time).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}</p>
-                <Button className="hidden lg:block" onClick={() => handleQueue(id)}>Queue</Button>
-                <Button className="hidden lg:block" onClick={() => handleReject(id)}>Reject</Button>
+                <Button className="hidden lg:block" onClick={() => handleQueue(id)}>{t('queue')}</Button>
+                <Button className="hidden lg:block" onClick={() => handleReject(id)}>{t('reject')}</Button>
                 <Button className="block lg:hidden" onClick={() => handleQueue(id)}><Plus/></Button>
                 <Button className="block lg:hidden" onClick={() => handleReject(id)}><Minus/></Button>
             </div>
--- i18n/request.ts
+++ i18n/request.ts
@@ -1,10 +1,9 @@
 import {getRequestConfig} from 'next-intl/server';
- 
+
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = 'en';
- 
+  // Use the navigator's language
+  const locale = (navigator.language || 'en').split('-')[0];
+
   return {
     locale,
     messages: (await import(`@/messages/${locale}.json`)).default
