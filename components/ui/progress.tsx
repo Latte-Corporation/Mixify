563b52219d15e8e0e8ce80cb22bba39f806a085d
Dorian TETU <doriantetu34@gmail.com>
2024-12-17 10:51:42 +0100

    Update Progress component to adjust transition duration and width calculation

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -18,8 +18,8 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all duration-700 ease-linear"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      className="h-full w-full flex-1 bg-primary transition-all duration-1000 ease-linear"
+      style={{ width: `${value ?? 0}%` }}
     />
   </ProgressPrimitive.Root>
 ))
